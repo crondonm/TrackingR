794a50da1d557f083d1cb95102506f87fee83ba5
Carlos Rondón-Moreno <31420139+crondonm@users.noreply.github.com>
2021-02-10 21:35:42 -0300

    Update update_test.sh

--- scripts/update_test.sh
+++ scripts/update_test.sh
@@ -35,64 +35,77 @@ python3 -W ignore -m construct_dataset.construct_dataset
 echo "Splicing database into subsets"
 python3 -W ignore -m construct_dataset.Build_parallel_datasets
 cp ./construct_dataset/output/dataset.csv    ../../fixed_revisions/derived_data/dataset/
-cp ./construct_dataset/output/dataset_AB.csv ../../fixed_revisions/derived_data/dataset/
-cp ./construct_dataset/output/dataset_CD.csv ../../fixed_revisions/derived_data/dataset/
-cp ./construct_dataset/output/dataset_EH.csv ../../fixed_revisions/derived_data/dataset/
-cp ./construct_dataset/output/dataset_IL.csv ../../fixed_revisions/derived_data/dataset/
-cp ./construct_dataset/output/dataset_MP.csv ../../fixed_revisions/derived_data/dataset/
-cp ./construct_dataset/output/dataset_QT.csv ../../fixed_revisions/derived_data/dataset/
+cp ./construct_dataset/output/dataset_A.csv  ../../fixed_revisions/derived_data/dataset/
+cp ./construct_dataset/output/dataset_B.csv  ../../fixed_revisions/derived_data/dataset/
+cp ./construct_dataset/output/dataset_C.csv  ../../fixed_revisions/derived_data/dataset/
+cp ./construct_dataset/output/dataset_D.csv  ../../fixed_revisions/derived_data/dataset/
+cp ./construct_dataset/output/dataset_EF.csv ../../fixed_revisions/derived_data/dataset/
+cp ./construct_dataset/output/dataset_GH.csv ../../fixed_revisions/derived_data/dataset/
+cp ./construct_dataset/output/dataset_IJ.csv ../../fixed_revisions/derived_data/dataset/
+cp ./construct_dataset/output/dataset_KL.csv ../../fixed_revisions/derived_data/dataset/
+cp ./construct_dataset/output/dataset_MN.csv ../../fixed_revisions/derived_data/dataset/
+cp ./construct_dataset/output/dataset_OP.csv ../../fixed_revisions/derived_data/dataset/
+cp ./construct_dataset/output/dataset_QR.csv ../../fixed_revisions/derived_data/dataset/
+cp ./construct_dataset/output/dataset_ST.csv ../../fixed_revisions/derived_data/dataset/
 cp ./construct_dataset/output/dataset_UZ.csv ../../fixed_revisions/derived_data/dataset/
 
-# Run unit tests on the constructed dataset
-cd ./tests/
-nosetests tests_dataset.py -v > tests_dataset.txt 2>&1
-cd ..
+# # Run unit tests on the constructed dataset
+# cd ./tests/
+# nosetests tests_dataset.py -v > tests_dataset.txt 2>&1
+# cd ..
 
-# Estimate R using Kalman filter to obtain
-# estimates of the variance of disturbances
-# and signal-to-noise ratio (used for calibrating
-# priors in Bayesian filtering with STAN)
+# # Estimate R using Kalman filter to obtain
+# # estimates of the variance of disturbances
+# # and signal-to-noise ratio (used for calibrating
+# # priors in Bayesian filtering with STAN)
 
-echo "Getting Kalman filter estimates"
-cd ./estimate_R
-rm -r ./input/
-mkdir ./input/
-mkdir ./input/estimate_R_KF/
-mkdir ./input/construct_STAN_models/
-mkdir ./input/estimate_R_STAN/
-cp ../../../fixed_revisions/derived_data/dataset/dataset.csv    ./input/estimate_R_KF/
-cp ../../../fixed_revisions/derived_data/dataset/dataset_AB.csv ./input/estimate_R_KF/
-cp ../../../fixed_revisions/derived_data/dataset/dataset_CD.csv ./input/estimate_R_KF/
-cp ../../../fixed_revisions/derived_data/dataset/dataset_EH.csv ./input/estimate_R_KF/
-cp ../../../fixed_revisions/derived_data/dataset/dataset_IL.csv ./input/estimate_R_KF/
-cp ../../../fixed_revisions/derived_data/dataset/dataset_MP.csv ./input/estimate_R_KF/
-cp ../../../fixed_revisions/derived_data/dataset/dataset_QT.csv ./input/estimate_R_KF/
-cp ../../../fixed_revisions/derived_data/dataset/dataset_UZ.csv ./input/estimate_R_KF/
-cd ..
-python3 -W ignore -m estimate_R.estimate_R_KF
-cp ./estimate_R/output/estimate_R_KF/optim_res.csv ../../fixed_revisions/derived_data/KF_results/
+# echo "Getting Kalman filter estimates"
+# cd ./estimate_R
+# rm -r ./input/
+# mkdir ./input/
+# mkdir ./input/estimate_R_KF/
+# mkdir ./input/construct_STAN_models/
+# mkdir ./input/estimate_R_STAN/
+# cp ../../../fixed_revisions/derived_data/dataset/dataset.csv    ./input/estimate_R_KF/
+# cp ../../../fixed_revisions/derived_data/dataset/dataset_A.csv  ./input/estimate_R_KF/
+# cp ../../../fixed_revisions/derived_data/dataset/dataset_B.csv  ./input/estimate_R_KF/
+# cp ../../../fixed_revisions/derived_data/dataset/dataset_C.csv  ./input/estimate_R_KF/
+# cp ../../../fixed_revisions/derived_data/dataset/dataset_D.csv  ./input/estimate_R_KF/
+# cp ../../../fixed_revisions/derived_data/dataset/dataset_EF.csv ./input/estimate_R_KF/
+# cp ../../../fixed_revisions/derived_data/dataset/dataset_GH.csv ./input/estimate_R_KF/
+# cp ../../../fixed_revisions/derived_data/dataset/dataset_IJ.csv ./input/estimate_R_KF/
+# cp ../../../fixed_revisions/derived_data/dataset/dataset_KL.csv ./input/estimate_R_KF/
+# cp ../../../fixed_revisions/derived_data/dataset/dataset_MN.csv ./input/estimate_R_KF/
+# cp ../../../fixed_revisions/derived_data/dataset/dataset_OP.csv ./input/estimate_R_KF/
+# cp ../../../fixed_revisions/derived_data/dataset/dataset_QR.csv ./input/estimate_R_KF/
+# cp ../../../fixed_revisions/derived_data/dataset/dataset_ST.csv ./input/estimate_R_KF/
+# cp ../../../fixed_revisions/derived_data/dataset/dataset_UZ.csv ./input/estimate_R_KF/
+# cd ..
+# python3 -W ignore -m estimate_R.estimate_R_KF
+# cp ./estimate_R/output/estimate_R_KF/optim_res.csv ../../fixed_revisions/derived_data/KF_results/
 
-# Estimate R using Bayesian filtering
-echo "Constructing STAN models"
-cd ./estimate_R
-cp ../../../fixed_revisions/derived_data/KF_results/optim_res.csv ./input/construct_STAN_models/
-cd ..
-python3 -W ignore -m estimate_R.construct_STAN_models
-cp ./estimate_R/output/construct_STAN_models/model_missing.pkl    ../../fixed_revisions/derived_data/STAN_models/
-cp ./estimate_R/output/construct_STAN_models/model_no_missing.pkl ../../fixed_revisions/derived_data/STAN_models/
-cd ../../scripts/
+# # Estimate R using Bayesian filtering
+
+# echo "Constructing STAN models"
+# cd ./estimate_R
+# cp ../../../fixed_revisions/derived_data/KF_results/optim_res.csv ./input/construct_STAN_models/
+# cd ..
+# python3 -W ignore -m estimate_R.construct_STAN_models
+# cp ./estimate_R/output/construct_STAN_models/model_missing.pkl    ../../fixed_revisions/derived_data/STAN_models/
+# cp ./estimate_R/output/construct_STAN_models/model_no_missing.pkl ../../fixed_revisions/derived_data/STAN_models/
+# cd ../../scripts/
 
 # echo "Submitting parallel routines to estimate Rt"
 
 # # Each database receives a code:
 
-qsub estimate_R_1.sh # 1: dataset_AB.csv
-qsub estimate_R_2.sh # 2: dataset_CD.csv
-qsub estimate_R_3.sh # 3: dataset_EH.csv
-qsub estimate_R_4.sh # 4: dataset_IL.csv
-qsub estimate_R_5.sh # 5: dataset_MP.csv
-qsub estimate_R_6.sh # 6: dataset_QT.csv
-qsub estimate_R_7.sh # 7: dataset_UZ.csv
+# qsub estimate_R_1.sh # 1: dataset_AB.csv
+# qsub estimate_R_2.sh # 2: dataset_CD.csv
+# qsub estimate_R_3.sh # 3: dataset_EH.csv
+# qsub estimate_R_4.sh # 4: dataset_IL.csv
+# qsub estimate_R_5.sh # 5: dataset_MP.csv
+# qsub estimate_R_6.sh # 6: dataset_QT.csv
+# qsub estimate_R_7.sh # 7: dataset_UZ.csv
 
-echo "Appending results into single file"
+# echo "Appending results into single file"
 
