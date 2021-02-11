9e657d312387f5d0bc95e8e68b120d88ffc62aa6
Carlos Rondón-Moreno <31420139+crondonm@users.noreply.github.com>
2021-02-10 22:56:30 -0300

    Update update_estimates.sh

--- scripts/update_estimates.sh
+++ scripts/update_estimates.sh
@@ -9,8 +9,10 @@
 
 echo "Download Data"
 sh update_data.sh        # Download Data
+
 echo "Construct Datasets and Estimate Priors"
 sh prior_estim.sh        # Prepare estimation: Construct databases + Run priors
+
 echo "Submit Estimations to Clusters"
 qsub -N job3 estimate_R_A.sh         
 qsub -N job4  estimate_R_B.sh
@@ -22,13 +24,22 @@ qsub -N job9  estimate_R_IJ.sh
 qsub -N job10  estimate_R_KL.sh
 qsub -N job11  estimate_R_MN.sh
 qsub -N job12  estimate_R_OP.sh
-qsub -N job14  estimate_R_QR.sh
-qsub -N job15  estimate_R_ST.sh
-qsub -N job16  estimate_R_UZ.sh
+qsub -N job13  estimate_R_QR.sh
+qsub -N job14  estimate_R_ST.sh
+qsub -N job15  estimate_R_UZ.sh
 
 echo "Append New Dataset"
+qsub -N job16 -hold_jid job3, job4, job5, job6, job7, job8, job9, job10, job11, job12, job13, job14, job15 Appenddata.sh
 
-
+#     This is possible by executing the command below:
+#     % qsub -N job1 [Script name]
+#     % qsub -N job2 -hold_jid job1 [Script name]
+#     % qsub -N job3 -hold_jid job1,job2 [Script name]
+#     Each job is set with a new name: 'job1', 'job2' and 'job3'. When 'job1' execution is complete, 'job2' is executed; when both 'job1' and 'job2' are complete, 'job3' is executed.
+#     The same setting for the execution order can be achieved by using the expression shown below:
+#     % qsub -N job1 [Script name]
+#     % qsub -N job2 -hold_jid job1 [Script name]
+#     % qsub -N job3 -hold_jid "job*" [Script name]
 
 
 # echo "Appending results into single file"
