71c707da60a72185b2059f031a75c378f77c5b04
simas-kucinskas <63259530+simas-kucinskas@users.noreply.github.com>
2021-03-27 10:59:15 +0100

    Added a model of intraweek seasonality

--- Replication Codes/code/python/estimate_R/construct_STAN_models.py
+++ Replication Codes/code/python/estimate_R/construct_STAN_models.py
@@ -18,10 +18,9 @@ from python.tools import (
 output_folder = './estimate_R/output/construct_STAN_models'
 input_folder = './estimate_R/input/construct_STAN_models'
 min_T = 20
-gamma = 1 / 7.0
-inflation_factor = 3.0 # Factor by which variances are inflated for priors
-mean_mu0    = 0.35 # Prior for initial state
-std_dev_mu0 = 0.50 
+inflation_factor = 1.0 # Factor by which variances are inflated for priors
+mean_mu0    = 0.25 # Prior for initial state: mean
+std_dev_mu0 = 0.15 # Prior for initial state: standard devitaion
 
 #########################################
 ## Calibrate priors using KF estimates ##
@@ -30,7 +29,15 @@ std_dev_mu0 = 0.50
 clean_folder(output_folder)
 
 df = pd.read_csv('{}/optim_res.csv'.format(input_folder))
-df['precision_irregular'] = 1 / df['sigma2_irregular']
+
+# The local-level model estimated by frequentist Kalman filtering
+# does not include a seasonal component. For calibration
+# of the priors for full estimation that includes a seasonal
+# component, we allocate fraction of the variance of the irregular 
+# component (estimated by frequentist Kalman filtering) to the "true" irregular
+# component, and remainder to the seasonal component
+df['precision_irregular'] = 0.50 * (1 / df['sigma2_irregular'])
+df['precision_seasonal']  = 0.50 * (1 / df['sigma2_irregular'])
 
 # Remove the World aggregate when calibrating
 mask = df['Country/Region'] == 'World'
@@ -38,7 +45,7 @@ df = df.loc[~mask, ]
 
 # Calibration step
 priors = []
-for var_name in ['precision_irregular', 'signal_to_noise']:
+for var_name in ['precision_irregular', 'precision_seasonal', 'signal_to_noise']:
   # Inflate variance of variables keeping mean constant
   df[var_name] = ((inflation_factor ** 0.5) * df[var_name] 
                   - (inflation_factor ** 0.5 - 1) * df[var_name].mean())
@@ -74,6 +81,8 @@ s_code_missing = """
     real mu[N];
     real<lower=0> precision_irregular;  // Precision (inverse of variance) of irregular component
     real<lower=0> signal_to_noise;      // Signal-to-noise ratio
+    real s[N];                          // Intraweek seasonality component
+    real<lower=0> precision_seasonal;   // Precision (inverse of variance) of seasonal component
   }}
   
   transformed parameters {{
@@ -89,9 +98,17 @@ s_code_missing = """
     precision_irregular ~ gamma({alpha_irregular}, {beta_irregular});
     signal_to_noise     ~ gamma({alpha_signal_to_noise}, {beta_signal_to_noise});
     mu_zero             ~ normal({mean_mu0}, {std_dev_mu0});
+    precision_seasonal  ~ gamma({alpha_seasonal}, {beta_seasonal});
   
     // initial state
-    mu[1] ~ normal(mu_zero, precision_level ^ (-0.5));  
+    mu[1] ~ normal(mu_zero, precision_level ^ (-0.5)); 
+
+    // intraweek seasonality component
+    for(i in 7:N) {{
+      s[i] ~ normal(-s[i-1]-s[i-2]-s[i-3]-s[i-4]-s[i-5]-s[i-6], precision_seasonal ^ (-0.5));
+      // Ensures that E(s[i] + s_[i-1] + ... s[i- 6]) = 0
+      // See https://tharte.github.io/mbt/mbt.html#sec-3-9
+    }} 
 
     // state equation
     for(i in 2:N) {{
@@ -100,7 +117,7 @@ s_code_missing = """
 
     // observation equation
     for(i in 1:N) {{
-      y[i] ~ normal(mu[i], precision_irregular ^ (-0.5));
+      y[i] ~ normal(mu[i] + s[i], precision_irregular ^ (-0.5));
     }}
   }}
 """  
@@ -109,6 +126,8 @@ s_code_missing = s_code_missing.format(mean_mu0 = mean_mu0,
                        std_dev_mu0 = std_dev_mu0,
                        alpha_irregular = priors.loc[priors['var_name'] == 'precision_irregular', 'alpha'].values[0],
                        beta_irregular = priors.loc[priors['var_name'] == 'precision_irregular', 'beta'].values[0],
+                       alpha_seasonal = priors.loc[priors['var_name'] == 'precision_seasonal', 'alpha'].values[0],
+                       beta_seasonal = priors.loc[priors['var_name'] == 'precision_seasonal', 'beta'].values[0],
                        alpha_signal_to_noise = priors.loc[priors['var_name'] == 'signal_to_noise', 'alpha'].values[0],
                        beta_signal_to_noise = priors.loc[priors['var_name'] == 'signal_to_noise', 'beta'].values[0])
 with open('{}/stan_code_missing.txt'.format(output_folder), 'w+') as text_file:
@@ -137,6 +156,8 @@ s_code_no_missing = """
     real mu[N];
     real<lower=0> precision_irregular;  // Precision (inverse of variance) of irregular component
     real<lower=0> signal_to_noise;      // Signal-to-noise ratio
+    real s[N];                          // Intraweek seasonality component
+    real<lower=0> precision_seasonal;   // Precision (inverse of variance) of seasonal component
   }}
   
   transformed parameters {{
@@ -151,9 +172,17 @@ s_code_no_missing = """
     precision_irregular ~ gamma({alpha_irregular}, {beta_irregular});
     signal_to_noise     ~ gamma({alpha_signal_to_noise}, {beta_signal_to_noise});
     mu_zero             ~ normal({mean_mu0}, {std_dev_mu0});
+    precision_seasonal  ~ gamma({alpha_seasonal}, {beta_seasonal});
   
     // initial state
-    mu[1] ~ normal(mu_zero, precision_level ^ (-0.5));  
+    mu[1] ~ normal(mu_zero, precision_level ^ (-0.5));
+
+    // intraweek seasonality component
+    for(i in 7:N) {{
+      s[i] ~ normal(-s[i-1]-s[i-2]-s[i-3]-s[i-4]-s[i-5]-s[i-6], precision_seasonal ^ (-0.5));
+      // Ensures that E(s[i] + s_[i-1] + ... s[i- 6]) = 0
+      // See https://tharte.github.io/mbt/mbt.html#sec-3-9
+    }}  
 
     // state equation
     for(i in 2:N) {{
@@ -162,7 +191,7 @@ s_code_no_missing = """
 
     // observation equation
     for(i in 1:N) {{
-      y[i] ~ normal(mu[i], precision_irregular ^ (-0.5));
+      y[i] ~ normal(mu[i] + s[i], precision_irregular ^ (-0.5));
     }}
   }}
 """  
@@ -171,6 +200,8 @@ s_code_no_missing = s_code_no_missing.format(mean_mu0 = mean_mu0,
                        std_dev_mu0 = std_dev_mu0,
                        alpha_irregular = priors.loc[priors['var_name'] == 'precision_irregular', 'alpha'].values[0],
                        beta_irregular = priors.loc[priors['var_name'] == 'precision_irregular', 'beta'].values[0],
+                       alpha_seasonal = priors.loc[priors['var_name'] == 'precision_seasonal', 'alpha'].values[0],
+                       beta_seasonal = priors.loc[priors['var_name'] == 'precision_seasonal', 'beta'].values[0],
                        alpha_signal_to_noise = priors.loc[priors['var_name'] == 'signal_to_noise', 'alpha'].values[0],
                        beta_signal_to_noise = priors.loc[priors['var_name'] == 'signal_to_noise', 'beta'].values[0])
 with open('{}/stan_code_no_missing.txt'.format(output_folder), 'w+') as text_file:
